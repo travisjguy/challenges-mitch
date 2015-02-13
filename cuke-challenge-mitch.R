d20a9f44d5b41d8e7b8c40a5bdaf1a9d8cb380c4
Mitch Walters <mjw246@ufl.edu>
2015-02-13 10:29:37 -0500

    add data

---
+++ cuke-challenge-mitch.R
@@ -0,0 +1,4 @@
+download.file("http://r-bio.github.io/data/holothuriidae-specimens.csv",
+              "data/holothuriidae-specimens.csv")
+download.file("http://r-bio.github.io/data/holothuriidae-nomina-valid.csv",
+              "data/holothuriidae-nomina-valid.csv")
